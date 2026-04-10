927e9e00745aa0b1e254399b2e2d1787dbe090a5
lbarberis-sportway <l.barberis@sportway.net>
2026-04-10 11:52:15 +0200

    chore: remove unused file and associated references

--- api/index.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# Aggiungi la root directory al path per permettere i package imports
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from server import app
-
-# Vercel needs the instance named 'app'
